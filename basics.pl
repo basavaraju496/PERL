edd7ba4da904f74fa180b97dd0967162e7b88060
matam basavaraju <57475017+basavaraju496@users.noreply.github.com>
2023-05-26 21:48:30 +0530

    Create basics.pl

---
+++ basics.pl
@@ -0,0 +1,16 @@
+1.Scalar
+
+Scalars are simple variables. 
+They are preceded by a dollar sign ($). 
+A scalar is either a number, a string, or a reference. 
+A reference is actually an address of a variable, which we will see in the upcoming chapters.
+
+2.Arrays
+
+Arrays are ordered lists of scalars that you access with a numeric index, which starts with 0. 
+They are preceded by an "at" sign (@).
+
+3.Hashes
+
+Hashes are unordered sets of key/value pairs that you access using the keys as subscripts. 
+They are preceded by a percent sign (%).
